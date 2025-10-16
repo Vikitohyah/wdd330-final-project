f71e0632e6d8154e438e89973e67f54e4d8170cb
Vikitohyah <victoriaedorh@gmail.com>
2025-10-16 04:22:53 +0100

    Made changes to viteconfig and package.json

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
-import { defineConfig } from 'vite';
+import { resolve } from "path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: 'src',
+  root: "src/",
+
   build: {
-    outDir: '../dist',
+    outDir: "../dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        watchlist: resolve(__dirname, "src/pages/watchlist.html"),
+        join: resolve(__dirname, "src/pages/join.html"),
+        watchnow: resolve(__dirname, "src/pages/watchnow.html"),
+        books: resolve(__dirname, "src/pages/books.html"),
+      },
+    },
   },
 });
